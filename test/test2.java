db7b0eca319a872fc7a40be223d52c98932d9044
Mickael Alvarez <alvarez.mickael.93@gmail.com>
2016-10-10 20:31:47 +0200

    user refactor (will be deleted)

--- test/test2.java
+++ test/test2.java
@@ -14,64 +14,50 @@ import org.eclipse.text.edits.MalformedTreeException;
 import org.eclipse.text.edits.TextEdit;
 
 public class StyleFormatter {
-    private CodeFormatter codeFormatter;
+	private CodeFormatter codeFormatter;
 
-    public StyleFormatter(
-            Map<String, String> options
-    ) {
-        this.codeFormatter = ToolFactory.createCodeFormatter(options);
-    }
+	public StyleFormatter(Map<String, String> options) {
+		this.codeFormatter = ToolFactory.createCodeFormatter(options);
+	}
 
-    public IDocument apply(
-            String filePath
-    ) throws MalformedTreeException, BadLocationException
-    {
-        String source = getSourceFile(filePath);
-        IDocument document = new Document(source);
-        TextEdit edit = getFormatter(source);
-        edit.apply(document);
-        return document;
-    }
+	public IDocument apply(String filePath) throws MalformedTreeException, BadLocationException {
+		String source = getSourceFile(filePath);
+		IDocument document = new Document(source);
+		TextEdit edit = getFormatter(source);
+		edit.apply(document);
+		return document;
+	}
 
-    private TextEdit getFormatter(
-            String source
-    )
-    {
-        return codeFormatter.format(CodeFormatter.K_COMPILATION_UNIT, source, 0, source.length(), 0,
-                System.getProperty("line.separator"));
-    }
+	private TextEdit getFormatter(String source) {
+		return codeFormatter.format(CodeFormatter.K_COMPILATION_UNIT, source, 0, source.length(), 0,
+				System.getProperty("line.separator"));
+	}
 
-    private String getSourceFile(
-            String filePath
-    )
-    {
-        StringBuilder sb = new StringBuilder();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(filePath));
-            String line = br.readLine();
-            while (line != null) {
-                sb.append(line);
-                line = br.readLine();
-            }
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return sb.toString();
-    }
+	private String getSourceFile(String filePath) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(filePath));
+			String line = br.readLine();
+			while (line != null) {
+				sb.append(line);
+				line = br.readLine();
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return sb.toString();
+	}
 
-    private void test(
-            String str
-    )
-    {
-        switch (str) {
-            case "a":
-                break;
-            case "b":
-                break;
-            default:
+	private void test(String str) {
+		switch (str) {
+		case "a":
+			break;
+		case "b":
+			break;
+		default:
 
-        }
-    }
+		}
+	}
 }
